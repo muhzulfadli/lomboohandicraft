395a2133b898889dbc3e32d998af682325af6379
Zulfadli <muh.fadli876@gmail.com>
2023-07-17 04:00:46 +0800

    add multiple pages

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,15 @@
 const menuBar = document.querySelector(".menu-bar");
 const menuNav = document.querySelector(".menu-navigation");
 
-menuBar.addEventListener("click", function() {
-    menuNav.classList.toggle("menu-active");
+menuBar.addEventListener("click", () => {
+  menuNav.classList.toggle("menu-active");
+});
+
+const navigation = document.querySelector(".navigation");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  navigation.classList.toggle("scrolling-active", windowPosition);
+  menuNav.classList.remove("menu-active");
 });
